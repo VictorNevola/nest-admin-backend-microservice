eac144e236cefeac6cc0212f38b27c3c035bd30b
Victor Nevola <victornevolapn@gmail.com>
2021-07-24 23:20:20 -0300

    create first event listening

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriaSchema } from './interfaces/categorias/categoria.schema';
+import { JogadorSchema } from './interfaces/jogadores/jogador.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot("mongodb://localhost:27017/sradmbackend", {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    }),
+    MongooseModule.forFeature([
+      { name: 'Categoria', schema: CategoriaSchema },
+      { name: 'Jogador', schema: JogadorSchema }
+    ])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import * as momentTimeZone from 'moment-timezone';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: [`amqp://user:${process.env.USERRABBIT}@${process.env.SERVERAWSEC2}/smartranking`],
+      queue: 'admin-backend'
+    },
+  });
+
+  Date.prototype.toJSON = function (): any {
+    return momentTimeZone(this).tz('America/Sao_Paulo').format('YYYY-MM-DD HH:mm:ss.SSS');
+  };
+
+  await app.listen();
 }
 bootstrap();
