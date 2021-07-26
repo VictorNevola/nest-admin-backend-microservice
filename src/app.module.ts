fb71e873f6cde7273bdbf493710a66b686d7cecd
Victor Nevola <victornevolapn@gmail.com>
2021-07-25 21:09:36 -0300

    Refactor code to modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CategoriaSchema } from './interfaces/categorias/categoria.schema';
-import { JogadorSchema } from './interfaces/jogadores/jogador.schema';
+import { CategoriasModule } from './categorias/categorias.module';
+import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
   imports: [
@@ -15,12 +13,8 @@ import { JogadorSchema } from './interfaces/jogadores/jogador.schema';
       useUnifiedTopology: true,
       useFindAndModify: false
     }),
-    MongooseModule.forFeature([
-      { name: 'Categoria', schema: CategoriaSchema },
-      { name: 'Jogador', schema: JogadorSchema }
-    ])
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    CategoriasModule,
+    JogadoresModule
+  ]
 })
 export class AppModule { }
